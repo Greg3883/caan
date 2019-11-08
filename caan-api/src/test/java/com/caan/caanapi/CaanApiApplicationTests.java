4cdc95cb1c9b1385c8062bfc185a5d8342ba1701
Greg3883 <gregoire.bejjaji@gmail.com>
2019-11-08 02:42:40 +0100

    Projet API Rest

---
+++ caan-api/src/test/java/com/caan/caanapi/CaanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caan.caanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
